bada1b9cb2539f5f9284965b4a545ea84824df21
Roger Campbell II <rc1336@gmail.com>
2019-01-09 08:58:01 -0500

    created a test.js file. My line of thinking is that if I move all of my js into one file I can call on the class with my key-value pairs easier than have them spread out across multiple js files.

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,29 @@ const myQuestions = require('./myQuestions')
 
 // messages.checkAnswer()
 
+const battleStarTriva = [
+    { 
+        question: 'At the site of the crashed Raptor on Kobol was a volatile situation; the Raptor was in flames and its passengers rushed to escape the wreckage. Gaius Baltar was caught behind a wall of fire. What happened that convinced him to escape?',
+        answers: {
+            a: 'Six convinced him to take her hand',
+            b: 'Helo dragged him out of the rackage ',
+            c: 'Commander Adama told him "to move his ass!"'
+        },
+        correctAnswer: 'a',
+        answerDetail: 'This event began Baltar\'s hallucinations on Kobol. Six called out to him to take her hand, and he had enough love for her to follow through with it. When he reached the exit, Crashdown broke him out of his trance and the two barely escaped before the Raptor exploded.'
+    },
+    {
+        question: 'As a side to Colonial Day, the Quorum of Twelve, the legislative body of the fleet, was in the process of being elected. President Roslin was seriously disturbed by the delegate who was selected to represent Sagittaron. Who had been chosen?',
+        answers: {
+            a: 'Kara Thrace',
+            b: 'Tom Zarek',
+            c: 'Lee Adama'
+        },
+        correctAnswer: 'b',
+        answerDetail: 'Tom Zarek, the terrorist who had blown up a government building on Sagittaron was selected to represent them. Many of the fleet had felt disenfranchised by the current leadership and thought Zarek would be the one to bring change. After the announcement of his office, he contacted the fleet from the Astral Queen and thanked those who had brought him to power.'
+    }
+]
+console.log(battleStarTriva[0].question)
 questionCreation.createQuestion()
 questionCreation.createAnswerBank()
 questionCreation.createQuizClass()
